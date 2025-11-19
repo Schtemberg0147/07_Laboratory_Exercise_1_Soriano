5c26fc7d26d3e2200d64e38db351dd820315d005
Schtemberg0147 <joseiiis013@gmail.com>
2025-11-19 19:11:18 +0800

    Add project files.

---
+++ 07_Laboratory_Exercise_1_Soriano/FrmClubRegistration.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace _07_Laboratory_Exercise_1_Soriano
+{
+    partial class FrmClubRegistration
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            label8 = new Label();
+            dataGridView1 = new DataGridView();
+            button3 = new Button();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(comboBox2);
+            panel1.Controls.Add(comboBox1);
+            panel1.Controls.Add(textBox5);
+            panel1.Controls.Add(textBox3);
+            panel1.Controls.Add(textBox4);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(label7);
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(662, 210);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(dataGridView1);
+            panel2.Controls.Add(button3);
+            panel2.Location = new Point(12, 243);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(783, 195);
+            panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Cursor = Cursors.Hand;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Location = new Point(688, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 36);
+            button1.TabIndex = 2;
+            button1.Text = "Register";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Cursor = Cursors.Hand;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Location = new Point(688, 68);
+            button2.Name = "button2";
+            button2.Size = new Size(100, 38);
+            button2.TabIndex = 2;
+            button2.Text = "Update";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(17, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(65, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Student ID:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(17, 89);
+            label2.Name = "label2";
+            label2.Size = new Size(64, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Last name:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(17, 152);
+            label3.Name = "label3";
+            label3.Size = new Size(31, 15);
+            label3.TabIndex = 1;
+            label3.Text = "Age:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(227, 89);
+            label4.Name = "label4";
+            label4.Size = new Size(65, 15);
+            label4.TabIndex = 1;
+            label4.Text = "First name:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(227, 152);
+            label5.Name = "label5";
+            label5.Size = new Size(48, 15);
+            label5.TabIndex = 1;
+            label5.Text = "Gender:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(425, 23);
+            label6.Name = "label6";
+            label6.Size = new Size(56, 15);
+            label6.TabIndex = 1;
+            label6.Text = "Program:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(425, 89);
+            label7.Name = "label7";
+            label7.Size = new Size(80, 15);
+            label7.TabIndex = 1;
+            label7.Text = "Middle name:";
+            label7.Click += label7_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Location = new Point(17, 41);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(182, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.BorderStyle = BorderStyle.FixedSingle;
+            textBox2.Location = new Point(17, 107);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(182, 23);
+            textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            textBox3.BorderStyle = BorderStyle.FixedSingle;
+            textBox3.Location = new Point(17, 170);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            textBox4.BorderStyle = BorderStyle.FixedSingle;
+            textBox4.Location = new Point(227, 107);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(173, 23);
+            textBox4.TabIndex = 2;
+            // 
+            // textBox5
+            // 
+            textBox5.BorderStyle = BorderStyle.FixedSingle;
+            textBox5.Location = new Point(425, 107);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(144, 23);
+            textBox5.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(425, 41);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(218, 23);
+            comboBox1.TabIndex = 3;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(227, 169);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(173, 23);
+            comboBox2.TabIndex = 3;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(39, 235);
+            label8.Name = "label8";
+            label8.Size = new Size(122, 15);
+            label8.TabIndex = 0;
+            label8.Text = "List Of Club Members";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(17, 18);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(645, 162);
+            dataGridView1.TabIndex = 0;
+            // 
+            // button3
+            // 
+            button3.Cursor = Cursors.Hand;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Location = new Point(676, 18);
+            button3.Name = "button3";
+            button3.Size = new Size(100, 38);
+            button3.TabIndex = 2;
+            button3.Text = "Refresh";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // FrmClubRegistration
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label8);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "FrmClubRegistration";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Panel panel2;
+        private Button button1;
+        private Button button2;
+        private Label label3;
+        private Label label5;
+        private Label label4;
+        private Label label2;
+        private ComboBox comboBox2;
+        private ComboBox comboBox1;
+        private TextBox textBox5;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label7;
+        private Label label6;
+        private DataGridView dataGridView1;
+        private Button button3;
+        private Label label8;
+    }
+}
---
+++ 07_Laboratory_Exercise_1_Soriano/FrmClubRegistration.cs
@@ -0,0 +1,15 @@
+namespace _07_Laboratory_Exercise_1_Soriano
+{
+    public partial class FrmClubRegistration : Form
+    {
+        public FrmClubRegistration()
+        {
+            InitializeComponent();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
